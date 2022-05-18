b8201d7fa8b60a99b7c7a93f42b53dd2a2c10072
Stachowiak <Stachowiak@POTWR>
2022-05-18 17:42:26 +0200

    Counter +4 -2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// interface Props {
+//   altText: string;
+// }
+
+//Zbudowac licznik, ktory sie sklada z 2 przyciskow "+" oraz "-".
+//Licznik na poczatku wyswietla wartosc 0
+//Po wcisnieciu "+" inkrementuje wartosc co 4
+//Po wcisnieciu "-" dekrementuje wartosc co 2
+//Dodatkowy przycisk "reset" powoduje powrot do wartosci 0
+
+//Wymysl swoj komponent, ktry robi cos z uzyciem hookow
+
+const Counter = () => {
+  const [Value, setValue] = React.useState(0);
   return (
-    <div className="App">
+    <div>
+      {Value}
+      <button onClick={() => setValue(Value + 4) }>+</button>
+      <button onClick={() => setValue(Value - 2) }>-</button>
+    </div>
+  )
+}
+
+const App = () =>
+  <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Counter />
       </header>
     </div>
-  );
-}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
