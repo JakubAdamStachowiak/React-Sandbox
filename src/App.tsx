9e9027559a096d4d0e801a5a0b480c2dfb8c9c57
JakubAdamStachowiak <kuba-stachowiak1@wp.pl>
2022-05-19 19:37:48 +0200

    ColorBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
@@ -16,19 +16,70 @@ import './App.css';
 
 const Counter = () => {
   const [Value, setValue] = React.useState(0);
+
   return (
     <div>
       {Value}
+      <br></br>
       <button onClick={() => setValue(Value + 4) }>+</button>
       <button onClick={() => setValue(Value - 2) }>-</button>
+      <button onClick={() => setValue(0)}>Reset</button>
     </div>
   )
+}
+
+const ColorBox = () => {
+    const [color, setColor] = React.useState("Red");
+    const [bgColor, setBgColor] = React.useState("Black");
+    const style = {color : color, background:bgColor};
+
+    return(
+      <div style={style}>
+        <label>Choose a font color: </label>
+          <select id="optionA">
+            <option value="Red">Red</option>
+            <option value="Green">Green</option>
+            <option value="Blue">Blue</option>
+            <option value="Yellow">Yellow</option>
+            <option value="White">White</option>
+            <option value="Black">Black</option>
+          </select>
+        <br></br>
+        <label>Choose a background color: </label>
+          <select id="optionB">
+            <option value="Red">Red</option>
+            <option value="Green">Green</option>
+            <option value="Blue">Blue</option>
+            <option value="Yellow">Yellow</option>
+            <option value="White">White</option>
+            <option value="Black">Black</option>
+          </select>
+          <br></br>
+          <button onClick={ () => { 
+            const inputA = document.getElementById('optionA') as HTMLInputElement;
+            const inputB = document.getElementById('optionB') as HTMLInputElement;
+            if(inputA?.value != null && inputB?.value != null){
+              if(inputA?.value != inputB?.value){
+                const valueA = inputA?.value;
+                const valueB = inputB?.value;
+                setColor(valueA);
+                setBgColor(valueB);
+              }
+              else{
+                alert('Colors need to be different!');
+              }
+            }
+          }
+          }>Ok</button>
+      </div>
+    )
 }
 
 const App = () =>
   <div className="App">
       <header className="App-header">
       <Counter />
+      <ColorBox/>
       </header>
     </div>
 
